16814172fd3a7514347a74710528dc3ce8006a7a
venish-karan <venishkg01@gmail.com>
2024-08-28 20:22:24 +0530

    strings, math, number

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
 console.log("Venish")
+
+const str = "venish";
+
+str.split("")
+
+console.log(str.startsWith("ven", 0))
+
+const num = 123.899
+
+console.log(num.toPrecision(3));
+
+
+const hundreds = 1000000
+
+console.log(hundreds.toLocaleString('en-IN'))
+
+console.log(Number.MAX_VALUE)
